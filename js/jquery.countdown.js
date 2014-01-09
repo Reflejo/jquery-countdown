d895923529cea41c8c9ddf2c2fe4fd40e5ce42e5
Everton Cardoso <verto@verto.me>
2014-01-09 09:48:39 -0300

    intervals array reset and clear actual interval main when recreate the counter

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -13,8 +13,10 @@ var createDigits = function(where, options) {
   // Iterate each startTime digit, if it is not a digit
   // we'll asume that it's a separator
   var mFirstPos, sFirstPos;
-  // reset digits array.
+  // reset digits and intervals array.
   digits = [];
+  intervals = [];
+
   for (var i = 0; i < options.startTime.length; i++) {
     if (parseInt(options.startTime[i]) >= 0) {
       elem = $('<div id="cnt_' + counter + '" class="cntDigit" />').css({
@@ -214,7 +216,10 @@ jQuery.fn.countdown = function(userOptions) {
     delete userOptions.endTime;
   }
   $.extend(options, userOptions);
-  createDigits(this, options);
-  intervals.main = setInterval(function(){ moveDigit(digits.length - 1, options); },
-                               1000);
+  if (this.length) {
+    clearInterval(intervals.main);
+    createDigits(this, options);
+    intervals.main = setInterval(function(){ moveDigit(digits.length - 1, options); },
+                                 1000);
+  }
 };
